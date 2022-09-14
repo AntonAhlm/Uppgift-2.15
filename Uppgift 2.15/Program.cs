2d994b698065a43adceeaa372687d6deb36c3265
Admin <Admin@TEB123012>
2022-09-14 12:15:10 +0200

    Add project files.

---
+++ Uppgift 2.15/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en mening med tre ord och med en punkt i slutet.");
+            Console.Write("Skriv här: ");
+
+            var mening = Console.ReadLine().Split(" ", ".");
+            string a = mening[0];
+            string b = mening[1];
+            string c = mening[2];
+
+            string c2 = mening[2];
+
+            Console.WriteLine(c + " " + b + " " + a);
+        }
+    }
+}
