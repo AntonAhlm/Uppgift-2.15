e4a8a62c4ca15262de0c40fd6e2b81095593b58c
AntonAhlm <113014145+AntonAhlm@users.noreply.github.com>
2022-09-16 08:57:48 +0200

    Update Program.cs

--- Uppgift 2.15/Program.cs
+++ Uppgift 2.15/Program.cs
@@ -6,17 +6,17 @@ namespace ____
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Skriv en mening med tre ord och med en punkt i slutet.");
-            Console.Write("Skriv här: ");
+            Console.WriteLine("Skriv en mening med tre ord och med en pungkula i slutet.");
+            Console.Write("Skriv här: sug dase")
 
-            var mening = Console.ReadLine().Split(" ", ".");
-            string a = mening[0];
-            string b = mening[1];
-            string c = mening[2];
+            var mening = Console.ReadLine().Split(" ", ".")
+            string a = mening[0]
+            string b = mening[1]
+            string c = mening[2]
 
-            string c2 = mening[2];
+            string c2 = mening[2]
 
-            Console.WriteLine(c + " " + b + " " + a);
+            Console.WriteLine(c + " " + b + " " + a)
         }
     }
 }
